8eed28bded7d0be5c3e138f22680344ab6d49dc5
thousands-star <51145299+thousands-star@users.noreply.github.com>
2024-09-11 23:04:18 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -36,7 +36,7 @@ def move_robot():
         ### if not using pid, just move the wheels as commanded
         if not use_pid:
             pibot.value = (left_speed, right_speed)          
-        
+            print('Value', left_encoder.value, right_encoder.value)
         ### with pid, left wheel is set as reference, and right wheel will try to match the encoder counter of left wheel
         ### pid only runs when robot moves forward or backward. Turning does not use pid
         else:
