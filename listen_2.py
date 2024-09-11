1ea1d4e4fc5d9d9ebcb0b2335798276d6e1178d0
thousands-star <51145299+thousands-star@users.noreply.github.com>
2024-09-12 00:48:42 +0800

    Update listen_2.py

--- listen_2.py
+++ listen_2.py
@@ -28,42 +28,44 @@ class Encoder(object):
 
 # main function to control the robot wheels
 def move_robot():
-    global use_pid, left_speed, right_speed, left_disp, right_disp
+    global use_pid, left_speed, right_speed, motion
+    global auto_flag, left_disp, right_disp, auto_motion
     flag_new_pid_cycle = True
-    auto_flag = False
 
     while True:
         if auto_flag is True:
-            if abs((left_disp+right_disp)/2) < 2:
-                motion = 'stop'
+            if abs((left_disp+right_disp)/2 - (left_encoder.value + right_encoder.value)/2) < 3:
+                auto_motion = 'stop'
                 pibot.value = (0,0)
                 auto_flag = False
                 continue
             else:
-                motion = 'forward'
+                auto_motion = 'forward'
                 pibot.value = (0.3,0.3)
-        ### if not using pid, just move the wheels as commanded
-        if not use_pid:
-            pibot.value = (left_speed, right_speed)          
-        ### with pid, left wheel is set as reference, and right wheel will try to match the encoder counter of left wheel
-        ### pid only runs when robot moves forward or backward. Turning does not use pid
         else:
-            if (motion == 'stop') or (motion == 'turning'):
-                pibot.value = (left_speed, right_speed) 
-                left_encoder.reset()
-                right_encoder.reset()
-                flag_new_pid_cycle = True          
-            else:
-                left_speed, right_speed = abs(left_speed), abs(right_speed)
-                if flag_new_pid_cycle:
-                    pid_right = PID(kp, ki, kd, setpoint=left_encoder.value, output_limits=(0,1), starting_output=right_speed)
-                    flag_new_pid_cycle = False
-                pid_right.setpoint = left_encoder.value
-                right_speed = pid_right(right_encoder.value)
-                if motion == 'forward': pibot.value = (left_speed, right_speed)
-                else: pibot.value = (-left_speed, -right_speed)
-                # print('Value', left_encoder.value, right_encoder.value)
-                # print('Speed', left_speed, right_speed)
+            print("auto_flag is never toggled")
+        ### if not using pid, just move the wheels as commanded
+        # if not use_pid:
+        #     pibot.value = (left_speed, right_speed)          
+        # ### with pid, left wheel is set as reference, and right wheel will try to match the encoder counter of left wheel
+        # ### pid only runs when robot moves forward or backward. Turning does not use pid
+        # else:
+        #     if (motion == 'stop') or (motion == 'turning'):
+        #         pibot.value = (left_speed, right_speed) 
+        #         left_encoder.reset()
+        #         right_encoder.reset()
+        #         flag_new_pid_cycle = True          
+        #     else:
+        #         left_speed, right_speed = abs(left_speed), abs(right_speed)
+        #         if flag_new_pid_cycle:
+        #             pid_right = PID(kp, ki, kd, setpoint=left_encoder.value, output_limits=(0,1), starting_output=right_speed)
+        #             flag_new_pid_cycle = False
+        #         pid_right.setpoint = left_encoder.value
+        #         right_speed = pid_right(right_encoder.value)
+        #         if motion == 'forward': pibot.value = (left_speed, right_speed)
+        #         else: pibot.value = (-left_speed, -right_speed)
+        #         # print('Value', left_encoder.value, right_encoder.value)
+        #         # print('Speed', left_speed, right_speed)
         time.sleep(0.005)
 
 # Receive confirmation whether to use pid or not to control the wheels (forward & backward)
