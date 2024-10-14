4f81af3ddeaefdc4c5efba207caa3cf8d755fe40
thousands-star <51145299+thousands-star@users.noreply.github.com>
2024-10-14 20:09:28 +0800

    Update listen_final.py

--- listen_final.py
+++ listen_final.py
@@ -112,7 +112,7 @@ def handle_mode1():
                     right_speed = pid_right(right_encoder.value)
                     left_speed = pid_left(left_encoder.value)
                     # print(f"Speed: {left_speed}, {right_speed}")
-                    pibot.value = (left_speed, right_speed)
+                    pibot.value = (right_speed, left_speed)
                 pibot.value = (0, 0)
             elif motion == "backward":
                 pid_left = PID(kp_lin, ki_lin, kd_lin, setpoint=left_disp, output_limits=(0.48,0.52), starting_output=linear_speed)
