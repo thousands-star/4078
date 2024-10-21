5177936aa91631845c0128f37628a99480616896
thousands-star <51145299+thousands-star@users.noreply.github.com>
2024-10-21 23:40:00 +0800

    c

--- listen_final.py
+++ listen_final.py
@@ -103,14 +103,14 @@ def handle_mode1():
             motion = "stop"
         finally:
             if motion == "forward":
-                pid_left = PID(kp_lin, ki_lin, kd_lin, setpoint=left_disp, output_limits=(0.55,0.75), starting_output=linear_speed)
-                pid_right =  PID(kp_lin, ki_lin, kd_lin, setpoint=right_disp, output_limits=(0.55,0.75), starting_output=linear_speed)
+                pid_left = PID(kp_lin, ki_lin, kd_lin, setpoint=left_disp, output_limits=(0.65,0.8), starting_output=linear_speed)
+                pid_right =  PID(kp_lin, ki_lin, kd_lin, setpoint=right_disp, output_limits=(0.65,0.8), starting_output=linear_speed)
                 while (left_encoder.value < abs(left_disp) - linear_tolerance) and (right_encoder.value < abs(right_disp) - linear_tolerance):
                     pid_left.setpoint = max(left_encoder.value, (right_encoder.value+left_encoder.value)/2)
                     pid_right.setpoint = max(right_encoder.value, (right_encoder.value+left_encoder.value)/2)
                     # print(f"Setpoint: {pid_left.setpoint}, {pid_right.setpoint}")
                     right_speed = pid_right(right_encoder.value) + 0.0
-                    left_speed = pid_left(left_encoder.value) + 0.015
+                    left_speed = pid_left(left_encoder.value) + 0.0
                     # print(f"Speed: {left_speed}, {right_speed}")
                     pibot.value = (left_speed, right_speed)
                 pibot.value = (0, 0)
