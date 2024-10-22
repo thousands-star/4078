27e81617969d130d555e1089acec300eac038b10
thousands-star <51145299+thousands-star@users.noreply.github.com>
2024-10-22 12:01:03 +0800

    Update listen_final_m4.py

--- listen_final_m4.py
+++ listen_final_m4.py
@@ -138,10 +138,10 @@ def handle_mode1():
                 while (left_encoder.value < abs(left_disp) - linear_tolerance) and (right_encoder.value < abs(right_disp) - linear_tolerance):
                     pid_left.setpoint = max(left_encoder.value, (right_encoder.value+left_encoder.value)/2)
                     pid_right.setpoint = max(right_encoder.value, (right_encoder.value+left_encoder.value)/2)
-                    print(f"Setpoint: {pid_left.setpoint}, {pid_right.setpoint}")
+                    #print(f"Setpoint: {pid_left.setpoint}, {pid_right.setpoint}")
                     right_speed = pid_right(right_encoder.value) + right_offset
                     left_speed = pid_left(left_encoder.value) + left_offset
-                    #print(f"Speed: {left_speed}, {right_speed}")
+                    print(f"Speed: {left_speed}, {right_speed}")
                     pibot.value = (left_speed, right_speed)
                 pibot.value = (0, 0)
             elif motion == "backward":
